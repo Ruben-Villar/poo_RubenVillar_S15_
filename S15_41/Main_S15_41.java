dbd4ea5849daddc491c94427249401f491c7fe50
villarpradar <131499129+Ruben-Villar@users.noreply.github.com>
2023-11-07 09:04:08 -0500

    Add files via upload

---
+++ S15_41/Main_S15_41.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S15_41;
+
+/**
+ *
+ * @author PCC08
+ */
+public class Main_S15_41{
+    public static  void main (String[] args){
+    
+    }
+}
---
+++ S15_41/estudiante_41.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S15_41;
+
+/**
+ *
+ * @author PCC08
+ */
+public class estudiante_41 {
+    //4 Atributos
+    int edad;
+    double estatura;
+    String colegio ;
+    String  colorpiel ;
+    
+    public void caminar(){
+        
+        System.out.println("Yo tengo de estatura: "+ estatura);
+    }
+     public void correr(){
+        
+         System.out.println("Mi colegio es: "+ colegio);
+       
+    }
+      public void ladrar(){
+        
+          System.out.println("Mi color de piel es : " + colorpiel);
+       
+    }
+       public void caracter(){
+        
+           System.out.println("Mi edad es: "+ edad);
+       
+    }
+}
